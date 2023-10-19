dd13b97bfa4192aaa3b557c1a95cf38ee4276966
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-10-19 15:20:38 +0200

    Add CRT part 2

--- src/main/java/no/obrien/twentytwo/CathodeRayTube.java
+++ src/main/java/no/obrien/twentytwo/CathodeRayTube.java
@@ -1,7 +1,6 @@
 package no.obrien.twentytwo;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 import no.obrien.utils.FileUtils;
 
@@ -43,6 +42,43 @@ public class CathodeRayTube {
     }
 
     return signalStrengthSum;
+  }
+
+  public String partTwo(String inputFilePath) {
+    List<String> lines = FileUtils.parseInputFile(inputFilePath)
+        .stream()
+        .filter(line -> !line.isEmpty())
+        .toList();
+
+    int pc = 0;
+    int x = 1;
+    int signalStrengthSum = 0;
+    boolean isDrawing = false;
+
+    for (int cycle = 1; cycle < 1000; cycle++) {
+      if (pc == lines.size()) {
+        break;
+      }
+      String line = lines.get(pc);
+      signalStrengthSum += calculateSignalStrength(cycle, x);
+      if (isDrawing) {
+        int argument = Integer.parseInt(line.split(" ")[1]);
+        x += argument;
+        isDrawing = false;
+        pc++;
+      } else {
+        String instruction = line.split(" ")[0];
+        if (instruction.equals("noop")) {
+          pc++;
+        } else if (instruction.equals("addx")) {
+          isDrawing = true;
+        } else {
+          throw new RuntimeException("Unknown instruction: " + instruction);
+        }
+      }
+    }
+
+    return "Merry Christmas!";
   }
 
   private int calculateSignalStrength(int cycleCount, int x) {
