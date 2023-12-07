81c58346b6e31fee8ba4b0088ae3ff587bb89929
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-12-07 09:52:38 +0100

    Add 2023-3-2

--- src/main/java/no/obrien/twentythree/GearRatios.java
+++ src/main/java/no/obrien/twentythree/GearRatios.java
@@ -1,94 +1,100 @@
 package no.obrien.twentythree;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
-import no.obrien.datastructures.Grid;
+import no.obrien.datastructures.Tuple;
 
 @UtilityClass
 public class GearRatios {
 
   public int partOne(List<String> lines) {
-    List<Integer> result = new ArrayList<>();
-    int row = 0;
-    int index = 0;
-    for (String line : lines) {
-      StringBuilder partNumber = new StringBuilder();
-      char[] chars = line.toCharArray();
-      boolean addPartNumber = false;
-      for (char c : chars) {
-        if (Character.isDigit(c)) {
-          partNumber.append(c);
-          // look left
-          if (index > 0) {
-            if (chars[index - 1] != '.' && !Character.isDigit(chars[index - 1])) {
-              addPartNumber = true;
-            }
-          }
-          // look right
-          if (index < chars.length - 1) {
-            if (chars[index + 1] != '.' && !Character.isDigit(chars[index + 1])) {
-              addPartNumber = true;
-            }
-          }
-          // look up
-          if (row > 0) {
-            String previousLine = lines.get(row - 1);
-            for (int i = Math.max(index - 1, 0); i <= Math.min(index + 1, line.length() - 1); i++) {
-              if (previousLine.charAt(i) != '.' && !Character.isDigit(previousLine.charAt(i))) {
-                addPartNumber = true;
-              }
-            }
-          }
-          // look down
-          if (row < lines.size() - 1) {
-            String nextLine = lines.get(row + 1);
-            for (int i = Math.max(index - 1, 0); i <= Math.min(index + 1, line.length() - 1); i++) {
-              if (nextLine.charAt(i) != '.' && !Character.isDigit(nextLine.charAt(i))) {
-                addPartNumber = true;
-              }
-            }
-          }
-          if (index == chars.length - 1 && addPartNumber) {
-            result.add(Integer.parseInt(partNumber.toString()));
-            addPartNumber = false;
-          }
-        } else {
-          if (addPartNumber) {
-            result.add(Integer.parseInt(partNumber.toString()));
-            addPartNumber = false;
-          }
-          partNumber.setLength(0);
+    Set<Tuple<Integer>> startCoords = new HashSet<>();
+    for (int y = 0; y < lines.size(); ++y) {
+      String line = lines.get(y);
+      for (int x = 0; x < line.length(); ++x) {
+        Tuple<Integer> coord = new Tuple<>(x, y);
+        char chr = line.charAt(x);
+        if (isSymbol(chr)) {
+          findTouchingDigits(coord).stream()
+              .filter(
+                  c -> isValid(lines, c)
+                      && Character.isDigit(lines.get(c.getY()).charAt(c.getX())))
+              .map(c -> findFirstChar(lines, c)).forEach(startCoords::add);
         }
-        index++;
       }
-      row++;
-      index = 0;
     }
-    return result.stream().mapToInt(Integer::intValue).sum();
+    return startCoords.stream().mapToInt(c -> readNumberFrom(lines, c)).sum();
   }
 
-  public int partTwo(List<String> lines) {
-    List<Integer> result = new ArrayList<>();
-    Grid<Character> grid = new Grid<>();
-    int x = 0;
-    int y = 0;
-    for (String line : lines) {
-      for (char c : line.toCharArray()) {
-        grid.add(x, y, c);
-        x++;
+  public long partTwo(List<String> lines) {
+    int sum = 0;
+    for (int y = 0; y < lines.size(); ++y) {
+      String line = lines.get(y);
+      for (int x = 0; x < line.length(); ++x) {
+        var coord = new Tuple<>(x, y);
+        char chr = line.charAt(x);
+        if (isSymbol(chr)) {
+          Set<Tuple<Integer>> candidates = findTouchingDigits(coord).stream()
+              .filter(
+                  c -> isValid(lines, c)
+                      && Character.isDigit(lines.get(c.getY()).charAt(c.getX())))
+              .map(c -> findFirstChar(lines, c))
+              .collect(Collectors.toSet());
+          if (candidates.size() == 2) {
+            sum += candidates.stream().mapToInt(c -> readNumberFrom(lines, c))
+                .reduce(1, (a, b) -> a * b);
+          }
+        }
       }
-      y++;
     }
-    // go row by row
-    x = 0;
-    y = 0;
-    for (int rowIndex = 1; rowIndex <= grid.getMaxY(); rowIndex++) {
-      for (int columnIndex = 0; columnIndex < grid.getMaxX(); columnIndex++) {
-        System.out.println(grid.get(columnIndex, rowIndex - 1));
-      }
-      y++;
+    return sum;
+  }
+
+
+  private List<Tuple<Integer>> findTouchingDigits(
+      Tuple<Integer> symbol) {
+    var result = new ArrayList<Tuple<Integer>>();
+
+    int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+    int[] dy = {1, 1, 1, 0, 0, -1, -1, -1};
+
+    for (int i = 0; i < dx.length; i++) {
+      int x = symbol.getX() + dx[i];
+      int y = symbol.getY() + dy[i];
+      result.add(new Tuple<>(x, y));
+
     }
-    return result.stream().mapToInt(Integer::intValue).sum();
+    return result.stream().toList();
+  }
+
+  boolean isValid(List<String> lines, Tuple<Integer> tuple) {
+    return 0 <= tuple.getY() && tuple.getY() < lines.size()
+        && 0 <= tuple.getX() && tuple.getX() < lines.get(tuple.getY()).length();
+  }
+
+  private static boolean isSymbol(char c) {
+    return c != '.' && !Character.isDigit(c);
+  }
+
+  private static Tuple<Integer> findFirstChar(List<String> lines, Tuple<Integer> coord) {
+    String line = lines.get(coord.getY());
+    int x = coord.getX();
+    while (x > 0 && Character.isDigit(line.charAt(x - 1))) {
+      --x;
+    }
+    return new Tuple<>(x, coord.getY());
+  }
+
+  private static int readNumberFrom(List<String> lines, Tuple<Integer> startCoord) {
+    String line = lines.get(startCoord.getY());
+    int x = startCoord.getX();
+    while (x < line.length() && Character.isDigit(line.charAt(x))) {
+      ++x;
+    }
+    return Integer.parseInt(line.substring(startCoord.getX(), x));
   }
 }
