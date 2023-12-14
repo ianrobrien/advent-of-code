d9e3922471bec2010091b43a99736e5dfef413b6
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-12-14 10:09:58 +0100

    Add 2023-14-1 (#36)

---
+++ src/test/java/no/obrien/twentythree/day14/ParabolicReflectorDishTest.java
@@ -0,0 +1,39 @@
+package no.obrien.twentythree.day14;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Collectors;
+import no.obrien.utils.FileUtils;
+import org.junit.jupiter.api.Test;
+
+class ParabolicReflectorDishTest {
+
+  private static final String INPUT_FILE = "twentythree/parabolicreflectordish.txt";
+
+  @Test
+  void testPartOne() {
+    assertEquals(107430,
+        ParabolicReflectorDish.partOne(FileUtils.parseInputFile(INPUT_FILE)));
+  }
+
+  @Test
+  void testPartOneSample() {
+    var input = """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....""";
+    assertEquals(136,
+        ParabolicReflectorDish.partOne(input.lines().collect(Collectors.toList())));
+  }
+
+  @Test
+  void testPartTwo() {
+  }
+}
